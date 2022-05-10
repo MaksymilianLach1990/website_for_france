48c55e96d281c9014ba8ee8a7086add46866f4dc
Krzysztof Lach <lachkrzysztof56@gmail.com>
2022-05-10 20:37:45 +0200

    Fix labels

--- myside/france/forms.py
+++ myside/france/forms.py
@@ -5,34 +5,19 @@ class ScenesCreateForm(forms.ModelForm):
     class Meta:
         model = Scenes
         fields = ('name', 'description', 'scene_image')
-        labels = {
-            'name': '',
-            'description': '',
-            'scene_image': '',
-        }
-
+        
 
 class PhraseCreateForm(forms.ModelForm):
     class Meta:
         model = Phrase
         fields = ('character_name', 'sentence')
-        labels = {
-            'character_name': '',
-            'sentence': '',
-        }
-
+        
 
 class WordCreateForm(forms.ModelForm):
     class Meta:
         model = Word
         fields = ('scenes', 'word_pl', 'word_fr', 'description', 'phonetic')
-        labels = {
-            'scenes': '',
-            'word_pl': '',
-            'word_fr': '',
-            'description': '',
-            'phonetic': '',
-        }
+        
 
 class SearchWordForm(forms.Form):
 
