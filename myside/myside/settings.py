f9c4a53d80ffed46aa3680cb4a406b4e3a5ad4b9
Krzysztof Lach <lachkrzysztof56@gmail.com>
2022-05-10 14:37:23 +0200

    Making the required chnges to the settings

--- myside/myside/settings.py
+++ myside/myside/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
     'france.apps.FranceConfig',
     'crispy_forms',
     'crispy_bootstrap5',
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'myside.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"main/templates/main"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "home"
+
+LOGOUT_REDIRECT_URL = "home"
